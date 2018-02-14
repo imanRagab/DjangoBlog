78e8b607f24640a0ab2ede13a22211a54fdd800f
Iman <iman.mansour94@gmail.com>
2018-02-14 23:33:10 +0200

    modifying base template

--- BlogV1/BlogV1/settings.py
+++ BlogV1/BlogV1/settings.py
@@ -106,4 +106,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = os.path.join(BASE_DIR, 'posts_app/static/')
