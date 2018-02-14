55c18516497cb74dd3800d3a691c24374a5f8ec4
Iman <iman.mansour94@gmail.com>
2018-02-14 17:11:34 +0200

    creating models

--- BlogV1/BlogV1/settings.py
+++ BlogV1/BlogV1/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'admin_app',
+    'auth_app',
+    'posts_app'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,8 +79,11 @@ WSGI_APPLICATION = 'BlogV1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pythonBlog',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': '1942'
     }
 }
 
--- BlogV1/posts_app/models.py
+++ BlogV1/posts_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=255)
+
+
+class Post(models.Model):
+    post_title = models.CharField(max_length=255)
+    post_content = models.TextField()
+    post_category = models.ForeignKey(Category)
+    post_picture = models.CharField(max_length=255)
+
+
+class Comment(models.Model):
+    comment_text = models.TextField()
+    #comment_post = models.ForeignKey(User)
+
