0eabbb879f6fab77f04b1284ff8723303895fe5a
Iman <iman.mansour94@gmail.com>
2018-02-15 03:05:47 +0200

    modifying views -- adding post page

--- BlogV1/posts_app/views.py
+++ BlogV1/posts_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from models import Post, Category, Comment, Tag, Reply, LikesDislikes
 
 # Create your views here.
+
+
+def post(request, post_id):
+    post = Post.objects.get(id = post_id)
+    categories = Category.objects.all()
+    comments = Comment.objects.filter(comment_post__id = post_id)
+
+    comments_replies = []
+
+    for comment in comments:
+        replies = Reply.objects.filter(reply_comment__id = comment.id)
+        comments_replies.append(replies)
+
+    context = {'post': post, 'categories': categories, 'comments':comments, 'replies':comments_replies}
+    return render(request, 'post.html', context)
