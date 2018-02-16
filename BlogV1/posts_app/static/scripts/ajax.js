cb82b35c3e4907e3d38329e120d64b657420021d
Iman <iman.mansour94@gmail.com>
2018-02-16 16:23:25 +0200

    putting categories script into ajax file

--- BlogV1/posts_app/static/scripts/ajax.js
+++ BlogV1/posts_app/static/scripts/ajax.js
@@ -108,6 +108,20 @@ $(function(){
 
     });
 
+    /////////////////////////////////////
+
+    $("#subs").click(function(){
+
+        if($(this).html() == "Subscribe")
+            $(this).html("UnSubscribe");
+
+        else
+            $(this).html("Subscribe");
+
+        $(this).toggleClass('btn-primary');
+        $(this).toggleClass('btn-danger');
+    });
+
 });
 
 
