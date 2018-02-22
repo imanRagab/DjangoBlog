b0a9027af5950549f3b9858ef93ca044544a9a1c
Youstina <youstinamasoud55@gmail.com>
2018-02-22 16:39:11 +0200

    admin panel posts-categories crud

--- BlogV1/admin_app/urls.py
+++ BlogV1/admin_app/urls.py
@@ -4,7 +4,8 @@ import views
 
 urlpatterns = [
 
-    url(r'^dashboard/$', views.dashboard),
+    # url(r'^dashboard/$', views.dashboard),
+    url(r'^$', views.allposts),
     url(r'^all/(?P<model>[a-z]+)', views.all),
     url(r'^add/(?P<model>[a-z]+)$', views.add),
     url(r'^edit/(?P<model>[a-z]+)/(?P<object_id>[0-9]+)$', views.edit),
@@ -12,6 +13,18 @@ urlpatterns = [
     url(r'^user/block/(?P<user_id>[0-9]+)$', views.block_user),
     url(r'^user/unblock/(?P<user_id>[0-9]+)$', views.unblock_user),
     url(r'^user/makeadmin/(?P<user_id>[0-9]+)$', views.make_admin),
+
+    url(r'^allposts/$', views.allposts),
+    url(r'^post_update/(?P<post_id>[0-9]+)/$', views.post_update),
+    url(r'^post_create/$', views.post_create),
+    url(r'^post_retrive/(?P<post_id>[0-9]+)/$', views.post_retrive),
+    url(r'^post_delete/(?P<post_id>[0-9]+)/$', views.post_delete),
+
+    url(r'^allcategory/$', views.allcategory),
+    url(r'^category_update/(?P<category_id>[0-9]+)/$', views.category_update),
+    url(r'^category_create/$', views.category_create),
+    url(r'^category_retrive/(?P<category_id>[0-9]+)/$', views.category_retrive),
+    url(r'^category_delete/(?P<category_id>[0-9]+)/$', views.category_delete),
 
 ]
 
--- BlogV1/admin_app/views.py
+++ BlogV1/admin_app/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render, redirect
 
 import time
@@ -19,17 +19,109 @@ def dashboard(request):
 
 ########################################
 
+def allposts(request):
+   all_posts = Post.objects.all()
+   context={
+       "allposts":all_posts
+   }
+   return render(request,'admin/allposts.html',context)
+################################################
+def post_create(request):
+    form=PostForm()
+    if request.method=="POST":
+        form = PostForm(request.POST , request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/ourblog/admin/allposts")
+    context={
+        "form":form,
+    }
+    return render(request,"admin/post_form.html",context)
+##################################################
+def post_retrive(request , post_id):
+    obj=Post.objects.get(id=post_id)
+    context={
+        "obj": obj,
+    }
+    return render(request,"admin/post_detail.html",context)
+######################################################
+def post_update(request,post_id):
+    obj = Post.objects.get(id=post_id)
+    #return HttpResponse(obj)
+    form = PostForm(instance=obj)
+
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=obj)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/ourblog/admin/allposts')
+
+    context={
+        "form":form
+    }
+    return render(request,'admin/post_update.html',context)
+###############################################
+def post_delete(request , post_id):
+    obj=Post.objects.get(id=post_id)
+    obj.delete()
+    return HttpResponseRedirect('/ourblog/admin/allposts')
+
+################################################
+################################################
+
+def allcategory(request):
+    all_category=Category.objects.all()
+    context={
+        "allcategory":all_category
+    }
+    return render(request,'admin/allcategory.html',context)
+#####################################################
+def category_create(request):
+    category_form=CategoryForm()
+    if request.method=="POST":
+        category_form= CategoryForm(request.POST)
+        if category_form.is_valid():
+            category_form.save()
+            return HttpResponseRedirect("/ourblog/admin/allcategory")
+    context={
+        "form":category_form,
+    }
+    return render(request,'admin/category_form.html',context)
+##########################################################
+def category_retrive(request , category_id):
+    obj=Category.objects.get(id=category_id)
+    context={
+        "obj":obj,
+    }
+    return render(request,'admin/category_detail.html',context)
+###########################################################
+def category_update(request,category_id):
+    obj=Category.objects.get(id=category_id)
+    form=CategoryForm(instance=obj)
+    if request.method=="POST":
+        form=CategoryForm(request.POST,instance=obj)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/ourblog/admin/allcategory')
+    context={
+        "form":form
+    }
+    return render(request,'admin/category_update.html',context)
+###############################################################
+def category_delete(request , category_id):
+    obj=Category.objects.get(id=category_id)
+    obj.delete()
+    return HttpResponseRedirect('/ourblog/admin/allcategory')
+###########################################################
+
+########################################
+
 def all(request, model):
 
     if(request.user.is_staff):
 
-        if(model == "category"):
-            objects = Category.objects.all()
 
-        elif(model == "post"):
-            objects = Post.objects.all()
-
-        elif (model == "tag"):
+        if (model == "tag"):
             objects = Tag.objects.all()
 
         elif(model == "comment"):
@@ -50,19 +142,115 @@ def all(request, model):
         elif (model == "subscription"):
             objects = CategorySubscribtion.objects.all()
 
-
         elif (model == "user"):
             objects = User.objects.all()
 
-        paginator = Paginator(objects, 10)  # Show 10 elements per page
-        if request.GET.get('page'):
-            page = request.GET.get('page')
-        else:
-            page = 1
-        page_objects = paginator.page(page)
-        context = {'objects': page_objects, 'model': model}
+        if object:
+            paginator = Paginator(objects, 10)  # Show 10 elements per page
+            if request.GET.get('page'):
+                page = request.GET.get('page')
+            else:
+                page = 1
+            page_objects = paginator.page(page)
+            context = {'objects': page_objects, 'model': model}
 
-        return render(request, 'admin/all.html', context)
+            return render(request, 'admin/all.html', context)
+
+        else:
+            raise Http404("Page does not exist")
+
+    else:
+        return HttpResponseRedirect('/ourblog/login/')
+
+
+#########################################################
+def add(request, model):
+
+    if (request.user.is_staff):
+
+        if (model == "forbidden"):
+            form = ForbiddenForm(request.POST)
+
+        elif (model == "user"):
+            form = UserRegForm(request.POST)
+
+        elif (model == "tag"):
+            form = TagForm(request.POST)
+
+        if form:
+            if request.method == "POST":
+                if form.is_valid():
+                    form.save()
+
+                    return HttpResponseRedirect('/ourblog/admin/all/' + model)
+
+            context = {"form": form, 'model': model}
+
+            return render(request, 'admin/add.html', context)
+
+        else:
+            raise Http404("Page does not exist")
+
+    else:
+        return HttpResponseRedirect('/ourblog/login/')
+
+
+##########################################################
+
+def edit(request, model, object_id):
+
+    if (request.user.is_staff):
+
+
+            if (model == "forbidden"):
+                try:
+                    object = Forbidden.objects.get(id=object_id)
+                except:
+                    raise Http404("Post does not exist")
+                form = ForbiddenForm(request.POST or None, instance=object)
+
+            elif (model == "user"):
+                try:
+                    object = User.objects.get(id=object_id)
+                except:
+                    raise Http404("Post does not exist")
+                form = UserRegForm(request.POST or None, instance=object)
+
+            else:
+                raise Http404("Post does not exist")
+
+
+            if request.method == "POST":
+                if form.is_valid():
+                    form.save()
+                    return HttpResponseRedirect('/ourblog/admin/all/' + model)
+
+            context = {"form": form, 'model': model, 'object_id': object_id}
+            return render(request, 'admin/edit.html', context)
+
+    else:
+        return HttpResponseRedirect('/ourblog/login/')
+
+
+##########################################################
+
+def delete(request, model, object_id):
+
+    if (request.user.is_staff):
+
+        try:
+
+            if (model == "forbidden"):
+                forbidden_word = Forbidden.objects.get(id=object_id)
+                forbidden_word.delete()
+
+            else:
+                raise Http404("Post does not exist")
+
+        except:
+            raise Http404("Post does not exist")
+
+        return redirect(request.META['HTTP_REFERER'])
 
     else:
         return HttpResponseRedirect('/ourblog/login/')
@@ -70,118 +258,64 @@ def all(request, model):
 
 #########################################################
 
-def add(request, model):
-
-    if(model == "category"):
-        form = CategoryForm(request.POST)
-
-    elif(model == "post"):
-        form = PostForm(request.POST, request.FILES or None)
-        # file_form = UploadFileForm(request.POST, request.FILES)
-
-    elif (model == "forbidden"):
-        form = ForbiddenForm(request.POST)
-
-    elif (model == "user"):
-        form = UserRegForm(request.POST)
-
-    if request.method == "POST":
-        if form.is_valid():
-            form.save()
-            # if file_form.is_valid():
-            #     file_dir = os.path.dirname(os.path.realpath('__file__'))
-            #     rel_path = "BlogV1/posts_app/static/images/posts_images" + time.time()
-            #     abs_file_path = os.path.join(file_dir, rel_path)
-            #     with open(abs_file_path, 'wb+') as destination:
-            #         for chunk in request.FILES['file'].chunks():
-            #             destination.write(chunk)
-
-            return HttpResponseRedirect('/ourblog/admin/all/' + model)
-
-    context = {"form": form, 'model': model}
-
-    return render(request, 'admin/add.html', context)
-
-
-##########################################################
-
-def edit(request, model, object_id):
-
-    if (model == "category"):
-        object = Category.objects.get(id=object_id)
-        form = CategoryForm(request.POST or None, instance=object)
-
-    elif (model == "post"):
-        object = Post.objects.get(id=object_id)
-        form = PostForm(request.POST or None, instance=object)
-
-    elif (model == "forbidden"):
-        object = Forbidden.objects.get(id=object_id)
-        form = ForbiddenForm(request.POST or None, instance=object)
-
-    elif (model == "user"):
-        object = User.objects.get(id=object_id)
-        form = UserRegForm(request.POST or None, instance=object)
-
-    if request.method == "POST":
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/ourblog/admin/all/' + model)
-
-    context = {"form": form, 'model': model, 'object_id': object_id}
-
-    return render(request, 'admin/edit.html', context)
-
-
-##########################################################
-
-def delete(request, model, object_id):
-    if (model == "category"):
-        category = Category.objects.get(id=object_id)
-        category.delete()
-
-    elif (model == "post"):
-        post = Post.objects.get(id=object_id)
-        post.delete()
-
-    elif (model == "forbiddenword"):
-        forbidden_word = Forbidden.objects.get(id=object_id)
-        forbidden_word.delete()
-
-    return redirect(request.META['HTTP_REFERER'])
-
-#########################################################
-
 def block_user(request, user_id):
 
-    user = User.objects.get(id=user_id)
-    user.is_active = 0
-    user.save()
+    if (request.user.is_staff):
 
-    return redirect(request.META['HTTP_REFERER'])
+        try:
+            user = User.objects.get(id=user_id)
+            user.is_active = 0
+            user.save()
+
+            return redirect(request.META['HTTP_REFERER'])
+
+        except:
+            raise Http404("Post does not exist")
+
+    else:
+        return HttpResponseRedirect('/ourblog/login/')
+
 
 #########################################################
 
 def unblock_user(request, user_id):
 
-    user = User.objects.get(id=user_id)
-    user.is_active = 1
-    user.save()
+    if (request.user.is_staff):
 
-    return redirect(request.META['HTTP_REFERER'])
+        try:
+            user = User.objects.get(id=user_id)
+            user.is_active = 1
+            user.save()
+
+        except:
+            raise Http404("Post does not exist")
+
+        return redirect(request.META['HTTP_REFERER'])
+
+
+    else:
+        return HttpResponseRedirect('/ourblog/login/')
+
 
 #########################################################
 
 def make_admin(request, user_id):
 
-    user = User.objects.get(id=user_id)
-    user.is_staff = 1
-    user.save()
+    if (request.user.is_staff):
 
-    return redirect(request.META['HTTP_REFERER'])
+        try:
+            user = User.objects.get(id=user_id)
+            user.is_staff = 1
+            user.save()
+
+        except:
+            raise Http404("Post does not exist")
+
+        return redirect(request.META['HTTP_REFERER'])
+
+
+    else:
+        return HttpResponseRedirect('/ourblog/login/')
 
 
 #########################################################
-
-# def error_404(request):
-#     return render(request, "error404")
